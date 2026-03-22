e0b8d30ef135b2f5c8f0b3469cf0a5e9fda64a1b
dylanj859-maker <dylanj859@gmail.com>
2026-03-22 13:57:23 -0400

    Add files via upload

---
+++ Week2/JS/main.js
@@ -0,0 +1,31 @@
+//JSON object
+const singers = [
+    {
+        name: "BBNO$",
+        dob: "6/30/1995",
+        songs: "1-800, edamame, two"
+    },
+    {
+        name: "Yung Gravy",
+        dob: "3/19/1996",
+        songs: "C'est La Vie, Betty, oops!"
+    },
+    {
+        name: "Ashnikko",
+        dob: "2/19/1996",
+        songs: "Paint The Town Blue, Slumber Party, Daisy"
+    }
+];
+
+const table = document.getElementById("singerTable");
+
+singers.forEach(singer => {
+    let row = `
+        <tr>
+            <td>${singer.name}</td>
+            <td>${singer.dob}</td>
+            <td>${singer.songs}</td>
+        </tr>
+    `;
+    table.innerHTML += row;
+});
